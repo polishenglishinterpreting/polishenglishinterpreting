29368d34fc5f56697de762c7b2ba3b2003e992eb
polishenglishinterpreting <beatak24@hotmail.com>
2018-11-24 11:12:29 +0000

    updated site

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                // url: "././mail/contact_me.php",
+                url: "https://formspree.io/matt@gizma.com",
                 type: "POST",
                 data: {
                     name: name,
