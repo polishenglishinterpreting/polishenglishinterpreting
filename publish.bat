f626c19a808ab5e6bfa5fa9587d385dcee72a355
polishenglishinterpreting <beatak24@hotmail.com>
2018-11-24 11:16:24 +0000

    updated site

---
+++ publish.bat
@@ -0,0 +1,7 @@
+cd C:\Beata.Website\polishenglishinterpreting\_site
+git init
+git remote add origin https://github.com/polishenglishinterpreting/polishenglishinterpreting.github.io
+type NUL > EmptyFile.txt
+git add .
+git commit -m "updated site"
+git push --force -u origin master
