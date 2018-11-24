af79fa3d33a5fd34f2fc2f85ab83d2ef7ee3fd0f
polishenglishinterpreting <beatak24@hotmail.com>
2018-11-24 14:27:00 +0000

    updated site

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,7 +19,7 @@ $(function() {
             }
             $.ajax({
                 // url: "././mail/contact_me.php",
-                url: "https://formcarry.com/s/O346U93YpW",
+                url: "https://formcarry.com/s/O346U93YpWD",
                 type: "POST",
                 dataType: "json",
                 data: {
