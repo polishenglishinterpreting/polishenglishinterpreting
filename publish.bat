afdf5a770a0323e19a6b48672df17ee88a7c1581
Matt Mulvaney <lufcmattylad@gmail.com>
2019-10-27 19:57:46 +0000

    updated site

--- publish.bat
+++ publish.bat
@@ -3,5 +3,7 @@ git init
 git remote add origin https://github.com/polishenglishinterpreting/polishenglishinterpreting.github.io
 type NUL > .nojekyll
 git add .
+git config --global user.email "lufcmattylad@gmail.com"
+git config --global user.name "Matt Mulvaney"
 git commit -m "updated site"
 git push --force -u origin master
